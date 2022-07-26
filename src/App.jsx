496dfecd0a142585ca82ad574a730fd43b47f66c
annekarolle <102996739+annekarolle@users.noreply.github.com>
2022-07-26 11:19:39 -0300

    fix/modificações na condição de render da api

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,20 @@ import Character from './components/Chacarcters';
 // import CharCard from './components/CharCard';
 
 
-function App() {
+ function App() {
 const [characterList , SetCharacterList ] = useState([])
 
-useEffect(() => {
+useEffect( ()  => {
   fetch("https://rickandmortyapi.com/api/character/")
   .then((response) => response.json())
   .then((response) => SetCharacterList(response))
-  .catch((err) => console.log(err))
+  .catch((err) => console.log('Erro dentro do fetch character: ', err))
 }, [])
 
   return (
     <div className="App">
       <div className="App-header">
-        <Character characterList={characterList.results} /> 
-   
+        <Character characterList={characterList.results} />    
       </div>
     </div>
   );
